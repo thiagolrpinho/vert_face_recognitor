d3d1fceb24aaab162181501768513cb1cf50d5d8
Thiago Luis <thiagoluisrpinho@gmail.com>
2019-10-23 15:49:32 -0300

    Need to fix acuracy

--- recognize_face.py
+++ recognize_face.py
@@ -35,46 +35,29 @@ face = crop_face(rgb)
 encodings = face_recognition.face_encodings(face, boxes)
  
 # initialize the list of names for each face detected
-names = []
+person_names = []
+person_encodings = []
 
+person_encodingsIdxs = [i for (i, b) in enumerate(data["names"]) if b == "thiago"]
+for i in person_encodingsIdxs:
+  person_encodings.append(data["encodings"][i])
+  person_names.append(data["names"][i])
 
 # loop over the facial embeddings
 for encoding in encodings:
   # attempt to match each face in the input image to our known
   # encodings
-  matches = face_recognition.compare_faces(data["encodings"], encoding)
-  name = "Unknown"
-
-# check to see if we have found a match
-  if True in matches:
-    # find the indexes of all matched faces then initialize a
-    # dictionary to count the total number of times each face
-    # was matched
-    matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-    counts = {}
- 
-    # loop over the matched indexes and maintain a count for
-    # each recognized face face
-    for i in matchedIdxs:
-      name = data["names"][i]
-      counts[name] = counts.get(name, 0) + 1
- 
-    # determine the recognized face with the largest number of
-    # votes (note: in the event of an unlikely tie Python will
-    # select first entry in the dictionary)
-    name = max(counts, key=counts.get)
+  matches = face_recognition.compare_faces(person_encodings, encoding)
   
-  # update the list of names
-  names.append(name)
-
-# loop over the recognized faces
-for ((top, right, bottom, left), name) in zip(boxes, names):
-  # draw the predicted face name on the image
-  cv2.rectangle(resized, (left, top), (right, bottom), (0, 255, 0), 2)
-  y = top - 15 if top - 15 > 15 else top + 15
-  cv2.putText(resized, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-    0.75, (0, 255, 0), 2)
+  if True in matches:
+    # loop over the recognized faces
+    for ((top, right, bottom, left), name) in zip(boxes, person_names):
+      # draw the predicted face name on the image
+      cv2.rectangle(resized, (left, top), (right, bottom), (0, 255, 0), 2)
+      y = top - 15 if top - 15 > 15 else top + 15
+      cv2.putText(resized, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
  
 # show the output image
 cv2.imshow("Image", resized)
 cv2.waitKey(0)
+
