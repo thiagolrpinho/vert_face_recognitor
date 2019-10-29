6c092662cf90b8a7ea793af914a6048137964afa
Thiago Luis <thiagoluisrpinho@gmail.com>
2019-10-29 11:38:21 -0300

    :sparkles: Add: Now dealing with unknown faces to be compared

--- compara_desconhecidos.py
+++ compara_desconhecidos.py
@@ -33,18 +33,26 @@ dados_comparacao = store_codes_with_names(unknown_encodings, unknown_names, unkn
 
 for pessoa_comparada in dados_comparacao.keys():
   # Primeiro procuramos em database pela pessoa a ser  comparada
-  dados_por_pessoa = dados_conhecidos[ pessoa_comparada ]
-  
-  for codigo_comparado, nome_arquivo_comparado in dados_comparacao [pessoa_comparada]:
-  # Depois iteramos pelas imagens a serem comparadas da mesma pessoa
-    matches = []
-    for codigo_conhecido, nome_arquivo_conhecido in dados_por_pessoa:
-      # Nós armazenamos os resultados em uma lista
-      matches.append( is_match( codigo_comparado, codigo_conhecido ) )
+  if pessoa_comparada in dados_conhecidos:
+    dados_por_pessoa = dados_conhecidos[ pessoa_comparada ]
+    
+    for codigo_comparado, nome_arquivo_comparado in dados_comparacao [pessoa_comparada]:
+    # Depois iteramos pelas imagens a serem comparadas da mesma pessoa
+      matches = []
+      for codigo_conhecido, nome_arquivo_conhecido in dados_por_pessoa:
+        # Nós armazenamos os resultados em uma lista
+        matches.append( is_match( codigo_comparado, codigo_conhecido ) )
 
-    if( matches.count(True) > len(matches)/2 ):
-      # Se mais que a metade dos rostos baterem, consideramos a pessoa como a mesma
-      print( "A imagem " + nome_arquivo_comparado + " é do " + pessoa_comparada )
-    else: 
-      print( "A imagem " + nome_arquivo_comparado + " não é do " + pessoa_comparada )
+      if( matches.count(True) > len(matches)/2 ):
+        # Se mais que a metade dos rostos baterem, consideramos a pessoa como a mesma
+        print( "A imagem " + nome_arquivo_comparado + " é do " + pessoa_comparada )
+      else: 
+        print( "A imagem " + nome_arquivo_comparado + " não é do " + pessoa_comparada )
+  else: 
+    # Caso a pessoa não seja encontrada na base de dados conhecida, ela não é comparada 
+    # e um aviso é emitido no terminal
+    dados_pessoa_sem_comparacao = dados_comparacao[pessoa_comparada]
+    print("A pessoa " + pessoa_comparada + " não foi encontrada na base de dados conhecida.")
+    for codigo_sem_comparacao, nome_arquivo_nao_comparado in dados_pessoa_sem_comparacao:
+      print("A imagem com nome " + nome_arquivo_nao_comparado + " não teve uma pessoa encontrada na database correspondente")
 
