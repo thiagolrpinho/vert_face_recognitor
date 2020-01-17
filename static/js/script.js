5e361d0ab4eeed3f1b2aeec66d5ca6311c8b3203
thiagobarros7 <thigb7@gmail.com>
2020-01-17 11:56:57 -0300

    :recycle: Refactor: Fixed back button and started to modularize javascript functions

--- static/js/script.js
+++ static/js/script.js
@@ -8,3 +8,27 @@ $(function() {
 function backButton(relative_path) {
     window.location.href= relative_path;
 }
+
+function inputLabel(id_list){
+  for (var i = 0; i < id_list.length; i++) {
+    
+    document.getElementById(id_list[i]).addEventListener('change',function(){
+      //get the file name
+      var fullPath = this.value;
+      var fileName = "arquivo_sem_nome"
+      if (fullPath) {
+        var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+        fileName = fullPath.substring(startIndex);
+        if (fileName.indexOf('\\') === 0 || fileName.indexOf('/') === 0) {
+          fileName = fileName.substring(1);
+        }
+      }
+      //replace the "Choose a file" label
+      document.getElementById('labelGroupFile0' + (i+1).toString()).textContent = fileName
+    })
+  }
+}
+
+$(document).ready(function() {
+    inputLabel(['first_image','second_image'])
+})
